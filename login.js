4116c16038f0537e68d1bad56d35b06fdce7e3ca
amartyam12 <amartyam12@gmail.com>
2022-09-25 03:50:06 +0530

    final

--- login.js
+++ login.js
@@ -4,8 +4,11 @@ function validate()
     var password=document.getElementById("password").value;
     if(username=="admin" && password=="1234")
     {
-       open("dashboard.html");
-
+    // //   open("dashboard.html")
+    //   var dashboard=window.open("", "_self");
+    //   dashboard.document.open("dashboard.html");
+    // window.open("dashboard.html", "_self");
+    window.location.href="http://127.0.0.1:5500/Verification/dashboard.html";
     }
     else{
         alert("Incorrect Username or Password");
