3410e421ac0660a4f6d23d570a821e21cb2da16e
amartyam12 <amartyam12@gmail.com>
2022-09-28 01:35:25 +0530

    new update

--- login.js
+++ login.js
@@ -2,9 +2,11 @@ function validate()
 {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
+    // var dashboard= <a href="http://127.0.0.1:5500/Verification/dashboard.html"></a>;
+    // var dashboard="http://127.0.0.1:5500/Verification/dashboard.html";
     if(username=="admin" && password=="1234")
     {
-    // //   open("dashboard.html")
+    //   open("dashboard.html");
     //   var dashboard=window.open("", "_self");
     //   dashboard.document.open("dashboard.html");
     // window.open("dashboard.html", "_self");
