5a8eda2bfab2b0ab429e4660ed0041d2449fd3ce
amartyam12 <amartyam12@gmail.com>
2022-10-20 20:07:59 +0530

    final

--- login.js
+++ login.js
@@ -2,6 +2,8 @@ function validate()
 {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
+    // var submit=document.getElementById("Login");
+    // var index=document.getElementsByClassName("index");
     // var dashboard= <a href="http://127.0.0.1:5500/Verification/dashboard.html"></a>;
     // var dashboard="http://127.0.0.1:5500/Verification/dashboard.html";
     if(username=="admin" && password=="1234")
@@ -15,4 +17,15 @@ function validate()
     else{
         alert("Incorrect Username or Password");
     }
+    // submit.addEventListener("click", (e) => {
+    //     e.preventDefault();
+    //     username = index.username.value;
+    //     password = index.password.value;
+    //     if (username === "admin" && password === "1234") { 
+    //         window.location.href="http://127.0.0.1:5500/Verification/dashboard.html";
+    //        }
+    //       else { 
+    //         alert("Incorrect Username or Password") ;
+    //     }
+    //     })
 }
